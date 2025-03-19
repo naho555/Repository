76275fd9a566f2ec61aa38f1576186da52323e5f
naho555 <na.5515nh@gmail.com>
2025-03-20 02:21:13 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,7 +1,9 @@
-# Java 17 を Render で使うための設定
+
 export JAVA_HOME=/opt/java/openjdk
 export PATH=$JAVA_HOME/bin:$PATH
 
-# Maven Wrapper を実行
+echo "JAVA_HOME is set to: $JAVA_HOME"
+java -version
+
 chmod +x LoginProject/mvnw
 LoginProject/mvnw clean package
