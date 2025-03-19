90909b6ea565aae1f0def0252a9c1d1a400861ca
naho555 <na.5515nh@gmail.com>
2025-03-20 02:35:16 +0900

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,4 @@
-echo "Starting build process..."
-set -x
-
-apt-get update && apt-get install -y openjdk-17-jdk
-
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export JAVA_HOME=/opt/render/project/.render/versions/java-17
 export PATH=$JAVA_HOME/bin:$PATH
 
 echo "JAVA_HOME is set to: $JAVA_HOME"
